328feb7b6b02c447206d2f3a2e9c19b81d0e9d1e
Alex Henrie <alexhenrie24@gmail.com>
2021-01-06 18:15:53 -0700

    Remove unused variable from Kumaraswamy.__init__

--- torch/distributions/kumaraswamy.py
+++ torch/distributions/kumaraswamy.py
@@ -37,7 +37,6 @@ class Kumaraswamy(TransformedDistribution):
 
     def __init__(self, concentration1, concentration0, validate_args=None):
         self.concentration1, self.concentration0 = broadcast_all(concentration1, concentration0)
-        finfo = torch.finfo(self.concentration0.dtype)
         base_dist = Uniform(torch.full_like(self.concentration0, 0),
                             torch.full_like(self.concentration0, 1))
         transforms = [PowerTransform(exponent=self.concentration0.reciprocal()),
